ae94d3e4aa7448b411cb4cb43f864279648c3008
laogia201 <o0oking.dark@gmail.com>
2025-06-18 18:55:26 +0700

    edit uploadImage3

--- src/controllers/pictureController.js
+++ src/controllers/pictureController.js
@@ -13,9 +13,6 @@ const uploadImage = async (req, res) => {
             return res.json({ status: 'error', message: 'Không có ảnh nào được upload!' });
         }
 
-        console.log('req.files:', req.files);
-        console.log('req.body:', req.body);
-
         const pictures = await Picture.insertMany(
             files.map(file => ({
                 folderId: folderId || undefined,
