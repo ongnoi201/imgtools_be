1214dd86b474f12fc74321731aa6b9735b633fca
laogia201 <o0oking.dark@gmail.com>
2025-06-18 22:20:22 +0700

    edit userRouter

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 
-router.get('/user/:id', auth, userController.getUser);
+router.get('/user', auth, userController.getUser);
 router.post('/add', userController.addUser);
 router.post('/login', userController.login);
 router.post('/logout', auth, userController.logout);
