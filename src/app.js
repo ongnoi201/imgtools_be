30720866e59ea427ef6cfcfd36cc314df29cae5f
laogia201 <o0oking.dark@gmail.com>
2025-06-18 18:57:09 +0700

    edit app

--- src/app.js
+++ src/app.js
@@ -10,14 +10,13 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectDB();
 app.use(cors());
+app.use(express.json());
+connectDB();
 
 app.use('/api/users', userRoutes);
 app.use('/api/pictures', pictureRoutes);
 app.use('/api/folders', folderRoutes);
-app.use(express.json());
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
