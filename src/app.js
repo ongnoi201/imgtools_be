96df43763dddd7bd90aef8edc02156de827e1b46
laogia201 <o0oking.dark@gmail.com>
2025-07-06 22:04:50 +0700

    edit app be

--- src/app.js
+++ src/app.js
@@ -11,7 +11,16 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+    origin: 'https://imgtools-kappa.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 connectDB();
 
